5d1fcd594cf9f38edcfe7581d608e63c2433b115
Eder <132967872+edeercm@users.noreply.github.com>
2023-11-11 14:10:47 -0600

    Update

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -7,14 +7,14 @@ import '../Contact/Contact.css'
 const contact = () => {
   return <>
     <section id='contact'>
-      <div className="container-xxl container-md container-sm d-flex align-items-center justify-content-center" style={{ width: '100%', height: '70vh' }}>
-        <div className="row contact-subcontainer">
-          <div className="col-lg-12 col-md-12 col-sm-12">
-            <div className='d-flex align-items-center justify-content-center text-center'>
-              <h2 className='contact-tittle'>Let's Connect</h2>
+      <div className="container-xxl container-md container-sm">
+        <div className="row">
+          <div className="side-left col-lg-6 col-md-12 col-sm-12">
+            <div className='contact-tittle'>
+              <h2>Let's Connect</h2>
             </div>
           </div>
-          <div className="col-lg-12 col-md-12 col-sm-12">
+          <div className="side-right col-lg-6 col-md-12 col-sm-12">
             <div className='d-flex justify-content-center'>
               <ContactForm />
             </div>
