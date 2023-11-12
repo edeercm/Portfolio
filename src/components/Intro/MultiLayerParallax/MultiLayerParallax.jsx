2af716f1ff79b77acc134f2af12d06fe10ef1030
Eder <132967872+edeercm@users.noreply.github.com>
2023-11-11 18:14:12 -0600

    Update

--- src/components/Intro/MultiLayerParallax/MultiLayerParallax.jsx
+++ src/components/Intro/MultiLayerParallax/MultiLayerParallax.jsx
@@ -1,6 +1,5 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import React, { useRef } from "react";
-import TypeAnimation from "../TypeAnimation/typeanimation";
 import '../MultiLayerParallax/MultiLayerParallax.css'
 
 
