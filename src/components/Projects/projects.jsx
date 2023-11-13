b3b7772233d69518a6871efbd61f28c91c6aad42
Eder <132967872+edeercm@users.noreply.github.com>
2023-11-13 14:01:25 -0600

    Update

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -25,51 +25,43 @@ const projects = () => {
           </div>
           <div className="col-lg-12 col-md-12 col-sm-12">
             <div className="d-flex flex-row align-items-center justify-content-center">
-
-
-              {/* <div data-aos="fade-up" data-aos-delay="500" data-aos-duration="1350" data-aos-easing="ease-in-out">
-                <div className="project-name" >
-                  <h5>SNKRS ECCOMERCE</h5>
-                </div>
-              </div> */}
-
-
-
               <div data-aos="fade-left" data-aos-delay="200" data-aos-duration="1450" data-aos-easing="ease-out">
                 <div className='d-flex align-items-center justify-content-evenly'>
-
-
                   <div data-aos="fade-up" data-aos-delay="500" data-aos-duration="1350" data-aos-easing="ease-in-out">
                     <div className="project-name" >
                       <h5>SNKRS ECCOMERCE</h5>
                     </div>
                   </div>
-
-
                   <img src={snkrs} alt="SNKRS Project" className='project-img' />
-
-
-                  {/* <div className='d-flex align-items-center justify-content-center'> */}
-                    <img src={snkrsmobile} alt="SNKRS Project" className='project-img-mobile' />
-                    {/* <div className="project-icon-mobile">
-                      <a href="https://snkrslabs.netlify.app/" target='_blank'><RxExternalLink /></a>
-                    </div> */}
-                  {/* </div> */}
-
-
+                  <img src={snkrsmobile} alt="SNKRS Project" className='project-img-mobile' />
                   <div className="project-icon">
                     <a href="https://snkrslabs.netlify.app/" target='_blank'><RxExternalLink /></a>
                   </div>
-
                 </div>
               </div>
-              {/* <div className="project-icon">
-                <a href="https://snkrslabs.netlify.app/" target='_blank'><RxExternalLink /></a>
-              </div> */}
             </div>
           </div>
           <div className="col-lg-12 col-md-12 col-sm-12 mt-5">
             <div className="d-flex flex-row align-items-center justify-content-center">
+              <div data-aos="fade-left" data-aos-delay="200" data-aos-duration="1450" data-aos-easing="ease-out">
+                <div className='d-flex align-items-center justify-content-evenly'>
+                  <div data-aos="fade-up" data-aos-delay="500" data-aos-duration="1350" data-aos-easing="ease-in-out">
+                    <div className="project-name" >
+                      <h5>TEXT ENCRYPTER</h5>
+                    </div>
+                  </div>
+                  <img src={encrypter} alt="SNKRS Project" className='project-img' />
+                  <img src={encryptermobile} alt="SNKRS Project" className='project-img-mobile' />
+                  <div className="project-icon">
+                    <a href="https://snkrslabs.netlify.app/" target='_blank'><RxExternalLink /></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
+            {/* <div className="d-flex flex-row align-items-center justify-content-center">
               <div data-aos="fade-up" data-aos-delay="500" data-aos-duration="1350" data-aos-easing="ease-in-out">
                 <div className="project-name" >
                   <h5>TEXT ENCRYPTER</h5>
@@ -89,7 +81,9 @@ const projects = () => {
               <div className="project-icon">
                 <a href="https://snkrslabs.netlify.app/" target='_blank'><RxExternalLink /></a>
               </div>
-            </div>
+            </div> */}
+
+
           </div>
         </div>
       </div>
