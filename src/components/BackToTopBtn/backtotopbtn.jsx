1dd8e65324f921c496e7db07ee4bf5d17db61421
Eder <132967872+edeercm@users.noreply.github.com>
2023-11-14 23:13:44 -0600

    Update

--- src/components/BackToTopBtn/backtotopbtn.jsx
+++ src/components/BackToTopBtn/backtotopbtn.jsx
@@ -1,67 +1,13 @@
-// import React, { useState, useEffect } from 'react';
-// import { MdKeyboardDoubleArrowUp } from "react-icons/md";
-// import '../BackToTopBtn/BackToTopBtn.css';
-
-// const BackToTopBtn = () => {
-  
-//   const [showButton, setShowButton] = useState(false);
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       const isBottom =
-//         window.innerHeight + window.scrollY >= document.documentElement.scrollHeight ||
-//         document.documentElement.clientHeight >= document.documentElement.scrollHeight;
-
-//       if (isBottom) {
-//         setShowButton(true);
-//       } else {
-//         setShowButton(false);
-//       }
-//     };
-
-//     window.addEventListener('scroll', handleScroll);
-
-//     return () => {
-//       window.removeEventListener('scroll', handleScroll);
-//     };
-//   }, []);
-
-//   const scrollToTop = () => {
-//     window.scrollTo({
-//       top: 0,
-//       behavior: 'smooth'
-//     });
-//   };
-
-//   return (
-//     <>
-//       {showButton && (
-//         <button id='backtotopbtn' onClick={scrollToTop}>
-//           <div className='d-flex flex-column align-items-center gap-1'>
-//             <MdKeyboardDoubleArrowUp />
-//             <div className='d-flex flex-column align-items-center lh-1'>
-//               <span>Top</span>
-//               <span>Back</span>
-//             </div>
-//           </div>
-//         </button>
-//       )}
-//     </>
-//   );
-// };
-
-// export default BackToTopBtn;
-
-
 import React, { useState, useEffect } from 'react';
-import { MdKeyboardDoubleArrowUp } from "react-icons/md";
+import { VscArrowSmallUp } from "react-icons/vsc";
 import '../BackToTopBtn/BackToTopBtn.css';
 
 const BackToTopBtn = () => {
-  
+
   const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
+
     const handleScroll = () => {
       const isBottom =
         window.innerHeight + window.scrollY >= document.documentElement.scrollHeight ||
@@ -71,7 +17,7 @@ const BackToTopBtn = () => {
       if (isBottom) {
         setTimeout(() => {
           setShowButton(true);
-        }, 2000); // Cambiar el valor del retraso según sea necesario
+        }, 1750); // Cambiar el valor del retraso según sea necesario
       } else {
         setShowButton(false);
       }
@@ -94,13 +40,10 @@ const BackToTopBtn = () => {
   return (
     <>
       {showButton && (
-        <button id='backtotopbtn' onClick={scrollToTop}>
-          <div className='d-flex flex-column align-items-center gap-1'>
-            <MdKeyboardDoubleArrowUp />
-            <div className='d-flex flex-column align-items-center lh-1'>
-              <span>Top</span>
-              <span>Back</span>
-            </div>
+        <button id='backtotopbtn' className={showButton ? 'show slide-and-fade' : ''} onClick={scrollToTop}>
+          <div className='d-flex flex-column align-items-center'>
+            <VscArrowSmallUp />
+            <span>Back To Top</span>
           </div>
         </button>
       )}
