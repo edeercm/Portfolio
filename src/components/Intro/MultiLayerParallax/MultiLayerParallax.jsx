e2e4c5e19af02309713a3cb5ba7e598f2d8633f8
Eder <132967872+edeercm@users.noreply.github.com>
2023-11-21 00:49:02 -0600

    Update

--- src/components/Intro/MultiLayerParallax/MultiLayerParallax.jsx
+++ src/components/Intro/MultiLayerParallax/MultiLayerParallax.jsx
@@ -2,18 +2,13 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import React, { useRef, useEffect, useState } from "react";
 import '../MultiLayerParallax/MultiLayerParallax.css'
 
-
 export default function MultiLayerParallax() {
-
   const ref = useRef(null);
 
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
-
-  // const textY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  // const textY2 = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
 
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 576);
 
@@ -30,18 +25,12 @@ export default function MultiLayerParallax() {
   }, []);
 
   const backgroundY = useTransform(scrollYProgress, [0, 1], isMobile ? ["0%", "15%"] : ["0%", "25%"]);
-  // const textY = useTransform(scrollYProgress, [0, 0.5], isMobile ? ["50%", "100%"] : ["-15%", "50%"]);
   const textY = useTransform(scrollYProgress, [0, 1], isMobile ? ["100%", "200%"] : ["0%", "200%"]);
-
- 
 
   return (
     <>
       <div className="parallax" ref={ref}>
         <div className="d-flex flex-column">
-          {/* <motion.h1 className="label" style={{ y: textY }}>
-          Welcome
-          </motion.h1> */}
           <motion.h1 className="label" style={{ y: textY }}>
             Welcome
           </motion.h1>
