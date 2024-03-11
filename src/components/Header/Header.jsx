37082abb0ef0def01e42d387314a61f411a9b03f
Eder <132967872+edeercm@users.noreply.github.com>
2024-03-11 05:29:33 -0600

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,13 +14,11 @@ const HeaderStyle = styled.header`
   background-color: var(--second-color);
 `;
 
-const Logo = styled.li`
- 
-    cursor: pointer;
-    font-weight: bold;
-    font-size: 2rem;
-    color: var(--third-color);
- 
+const Logo = styled.article`
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 2rem;
+  color: var(--third-color);
 `;
 
 const Header = () => {
@@ -30,11 +28,9 @@ const Header = () => {
       <div className="container">
         <div className="row">
           <div className="col-6">
-            <nav>
-              <ul>
-                <Logo><Link to={'/'}>X</Link></Logo>
-              </ul>
-            </nav>
+            <Logo>
+              <Link to={'/'}>X</Link>
+            </Logo>
           </div>
           <div className="col-6 d-flex justify-content-end align-items-center">
             <HeaderLabels />
