4022113dc70ef527b1405c1dfeccfcd6128ea55a
Eder <132967872+edeercm@users.noreply.github.com>
2024-03-11 05:27:45 -0600

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
-import Burger from './Burger';
-
 import HeaderLabels from './HeaderLabels';
+import Burger from './Burger';
 
 const HeaderStyle = styled.header`
   position: fixed;
   display: flex;
   align-items: center;
-  justify-content: center;
   width: 100%;
   height: 10vh;
   z-index: 10;
   background-color: var(--second-color);
 `;
 
-const Logo = styled.div`
-  cursor: pointer;
-  font-weight: bold;
-  font-size: 2rem;
-  color: var(--third-color);
-
-  @media (max-width: 576px) {
-  }
+const Logo = styled.li`
+ 
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 2rem;
+    color: var(--third-color);
+ 
 `;
 
 const Header = () => {
@@ -33,9 +30,11 @@ const Header = () => {
       <div className="container">
         <div className="row">
           <div className="col-6">
-            <Link to={'/'}>
-              <Logo>X</Logo>
-            </Link>
+            <nav>
+              <ul>
+                <Logo><Link to={'/'}>X</Link></Logo>
+              </ul>
+            </nav>
           </div>
           <div className="col-6 d-flex justify-content-end align-items-center">
             <HeaderLabels />
