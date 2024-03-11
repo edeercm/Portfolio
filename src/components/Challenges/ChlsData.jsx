f2b23fd5e1dd6cfa0a87a2f5e65cfb3f466487e7
Eder <132967872+edeercm@users.noreply.github.com>
2024-03-11 02:36:20 -0600

    Update

--- src/components/Challenges/ChlsData.jsx
+++ src/components/Challenges/ChlsData.jsx
@@ -1,6 +1,7 @@
 import chlsone from '../../assets/images/challenges/chlsone.png';
 import chlstwo from '../../assets/images/challenges/chlstwo.png';
 import chlsthree from '../../assets/images/challenges/chlsthree.png';
+import chlsfour from '../../assets/images/challenges/chlsfour.png';
 
 const chlsData = [
   {
@@ -26,6 +27,14 @@ const chlsData = [
     "desc": "Multi-page website developed with React, Bootstrap, styled-componets and react-router-dom.",
     "link": "https://spacetourismproject.netlify.app/",
     "image": chlsthree
+  },
+  {
+    "id": 4,
+    "title": "IP Addr Tracker",
+    "subtitle": "Frontend Mentor",
+    "desc": "To build this app i am using the IP Geolocation API by IPify, bootstrap and styled-componets. ",
+    "link": "https://ipaddresstrk.netlify.app/",
+    "image": chlsfour
   },
 ];
 
