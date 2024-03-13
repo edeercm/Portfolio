d261add2a10a1752f25009989ba768b6de40b9d6
Eder <132967872+edeercm@users.noreply.github.com>
2024-03-12 23:17:28 -0600

    Update burger animation

--- src/components/Header/Burger.jsx
+++ src/components/Header/Burger.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import BurgerLabels from './BurgerLabels';
 import BurgerIcons from './BurgerIcons';
 import { IoMdClose } from "react-icons/io";
 import { HiBars3BottomRight } from 'react-icons/hi2';
 
-const HamBtn = styled.button`
+const OffCStyle = styled.div`
+  background-color: var(--second-color);
+  transition: transform 0.45s cubic-bezier(0.5, 0.25, 0.75, 1), visibility 0.45s cubic-bezier(0.25, 0.1, 0.25, 1);
+`
+
+const BurgerBtn = styled.button`
  svg {
   font-size: 2.75rem;
   color: var(--third-color);
@@ -18,26 +23,24 @@ const Burger = () => {
     const offcanvas = document.getElementById('offcanvasRight');
     const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvas);
     offcanvasInstance.hide(); // Cerrar el offcanvas
+    setIsOpen(false);
   };
 
   return <>
     <div className='d-flex d-md-none'>
-      <HamBtn
+      <BurgerBtn
         type="button"
         data-bs-toggle="offcanvas"
         data-bs-target="#offcanvasRight"
         aria-controls="offcanvasRight"
       >
         <HiBars3BottomRight />
-      </HamBtn>
-      <div
+      </BurgerBtn>
+      <OffCStyle 
         className="offcanvas offcanvas-end"
         tabIndex="-1"
         id="offcanvasRight"
         aria-labelledby="offcanvasRightLabel"
-        style={{
-          backgroundColor: 'var(--second-color)'
-        }}
       >
         <div className="offcanvas-header d-flex justify-content-end">
           <button
@@ -54,10 +57,10 @@ const Burger = () => {
           </button>
         </div>
         <div className="offcanvas-body d-flex flex-column gap-5">
-          <BurgerLabels closeOffcanvas={closeOffcanvas}/>
+          <BurgerLabels closeOffcanvas={closeOffcanvas} />
           <BurgerIcons />
         </div>
-      </div>
+      </OffCStyle>
     </div>
 
   </>
