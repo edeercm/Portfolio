a407a1a897ce50eeb1b18f676df04a8607f4554c
Eder <132967872+edeercm@users.noreply.github.com>
2026-05-21 19:56:09 -0600

    Update mobile Header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,5 +1,5 @@
 import NavLinks from './components/desktop/NavLinks';
-import NavCTA from './components/desktop/NavCTA';
+import NavCTA from './components/NavCTA';
 import HamburgerButton from './components/mobile/HamburgerButton';
 import MobileDrawer from './components/mobile/MobileDrawer';
 import useActiveSection from './hooks/useActiveSection';
@@ -11,7 +11,33 @@ const Header = () => {
 
   return (
     <>
-      <nav className="fixed top-4 left-1/2 -translate-x-1/2 rounded-full px-6 py-2 bg-surface-container/60 backdrop-blur-xl border border-white/10 shadow-[0px_0px_20px_rgba(20,110,245,0.1)] flex items-center gap-gutter max-w-fit mx-auto z-50">
+      <nav 
+      className="
+        fixed 
+        top-4 
+        left-1/2 
+        -translate-x-1/2 
+
+        w-[90%]
+        md:w-[40%]
+
+        px-6 
+        py-3 
+
+        rounded-full 
+        bg-surface-container/60 
+        backdrop-blur-xl 
+        border 
+        border-white/10 
+        shadow-[0px_0px_20px_rgba(20,110,245,0.1)] 
+
+        flex 
+        items-center 
+        justify-between
+
+        z-50
+      "
+      >
 
         <HamburgerButton isOpen={isOpen} toggle={toggle} />
         <NavLinks activeSection={activeSection} />
